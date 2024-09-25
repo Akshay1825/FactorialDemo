a3e4778124ef7b3132095f00bde31ba654bcee5d
Akshay Utekar <akshayutekar1825@gmail.com>
2024-09-25 22:44:03 +0530

    Add project files.

---
+++ FactorialDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using FactorialDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial factorial = new Factorial();
+
+            Console.WriteLine("Enter a number :");
+            int number = int.Parse(Console.ReadLine());
+
+            int fact = factorial.CalculateFactorial(number);
+
+            Console.WriteLine($"The factorial of {number} is {fact}.");
+        }
+    }
+}
